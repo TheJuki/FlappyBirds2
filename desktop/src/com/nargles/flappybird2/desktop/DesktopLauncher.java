ded0b5e0fe317925f7fdeabccb281c796f2cb2b6
Justin Kirk <thejuki@hotmail.com>
2016-03-20 15:55:57 -0500

    Add icons

--- desktop/src/com/nargles/flappybird2/desktop/DesktopLauncher.java
+++ desktop/src/com/nargles/flappybird2/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.nargles.flappybird2.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.nargles.flappybird2.FlappyBirds2;
@@ -9,6 +10,11 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.height = 1080 / 3;
 		config.width = 1920 / 3;
+		config.title = "Flappy Bird II";
+		config.addIcon("data/icon_128.png", FileType.Internal);
+		config.addIcon("data/icon_64.png", FileType.Internal);
+		config.addIcon("data/icon_32.png", FileType.Internal);
+		config.addIcon("data/icon_16.png", FileType.Internal);
 		new LwjglApplication(new FlappyBirds2(), config);
 	}
 }
